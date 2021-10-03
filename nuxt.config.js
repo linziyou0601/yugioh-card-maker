bd2f72de0460a30a6302c3415a8458cf940de878
林子佑 <qqq23939889@gmail.com>
2021-10-03 11:11:39 +0800

    update: 更新base dir

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,7 +1,7 @@
 // only add `router.base = '/<repository_name>/'` if `DEPLOY_ENV` is `GH_PAGES`
 const routerBase = process.env.DEPLOY_ENV === 'GH_PAGES' ? {
   router: {
-    base: '/yugioh-card-maker/'
+    base: ''
   }
 } : {}
 
